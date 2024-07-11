eeb514003891dd6b5f0b9122f530e00345c9de9c
ipunganwar <ipung.anwar1@gmail.com>
2024-07-11 15:13:29 +0700

    chore: add mysql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'mysql_db',
+      port: 3307,
+      database: 'nestjs_docker_tutorial',
+      entities: [],
+      username: 'testuser',
+      password: 'testuser',
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
